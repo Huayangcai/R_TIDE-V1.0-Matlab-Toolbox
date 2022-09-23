bac7591ea21b425f15b4d618ff3b7c54d6b20105
Huayangcai <78842662+Huayangcai@users.noreply.github.com>
2022-09-23 11:34:08 +0800

    Update Qztidefun.m

--- Qztidefun.m
+++ Qztidefun.m
@@ -10,6 +10,7 @@ function stdmin=Qztidefun(q,z,t,fu,fband,cof,f,v,Qc)
         st(iz,k)=nan;ft(iz,k)=nan;y2(iz,k)=nan;Eta(iz,1:n,k)=nan;Phi(iz,1:n,k)=nan;
         time=t(iz,1);m=find(isnan(z1));z1(m)=[];time(m)=[];
         q1(m)=[];
+        m=find(q1>Qc(1,1));ff(1:length(q1),1)=1;ff(m)=Qc(1,1)./q1(m);%+1.7e-6*(q1(m)-Qc);
 
      for i=1:length(fband)
        m=find(fu>fband(i,1) & fu<fband(i,2));
@@ -19,11 +20,11 @@ function stdmin=Qztidefun(q,z,t,fu,fband,cof,f,v,Qc)
          pr(m)=cof(k,i+2);
        x1(:,m)=[f(m,ones(1,length(time)))]'.*cos(time*2*pi*[fu(m)]'*24+[v(m,ones(1,length(time)))]');
        x2(:,m)=[f(m,ones(1,length(time)))]'.*sin(time*2*pi*[fu(m)]'*24+[v(m,ones(1,length(time)))]');
-       Q1=(q1.^cof(k,i+2));
+       Q1=((q1.*ff).^cof(k,i+2));
        x3(:,m)=Q1(:,ones(1,length(m))).*x1(:,m);
        x4(:,m)=Q1(:,ones(1,length(m))).*x2(:,m);
      end
-     m=find(q1>Qc);x1(m,:)=0;x2(m,:)=0;x3(m,:)=0;x4(m,:)=0;
+     m=find(q1>Qc(1,2));x1(m,:)=0;x2(m,:)=0;x3(m,:)=0;x4(m,:)=0;
        xx=[ones(length(x1),1) q1.^pl  x1 x2 x3 x4];
        y=z1;b1 = regress(y,xx);%regression model to determine coefficients d and a
        y1=xx*b1; %reconstruct water level y1
